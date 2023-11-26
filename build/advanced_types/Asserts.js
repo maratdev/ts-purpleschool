bb7fa98c26ae1b1f7e99bc3d790f1a14aa12e9a7
Marat <starspersona@gmail.com>
2023-11-26 17:02:50 +0300

    Delete build directory

--- build/advanced_types/Asserts.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let payment = 10;
-payment = { sum: 10 };
-//# sourceMappingURL=Asserts.js.map
--- build/advanced_types/Void.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//void автоматически выводится, когда внутри функции нет инструкции return или она пустая
-function noop() {
-    // внутри пусто
-}
-// function foo(): void {
-//   return true; // Error!
-// }
-//# sourceMappingURL=Void.js.map
